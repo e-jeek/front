d3d0b53b0c76afd1a4783d13c493409bc0db8ad3
bondyuu <87316155+bondyuu@users.noreply.github.com>
2024-07-14 18:05:25 +0900

    Add: PWA 적용

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = withPWA({
+    reactStrictMode: false
+});
 
 export default nextConfig;
