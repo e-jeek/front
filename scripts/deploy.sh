5fe765a832928746840115eb23ed16728e9f3e2f
bondyuu <87316155+bondyuu@users.noreply.github.com>
2024-07-18 13:44:36 +0900

    fix: docker 실행 위치 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu
-sudo docker build -t docker-test -f ./action/Dockerfile .
+cd /home/ubuntu/action
+sudo docker build -t docker-test -f ./Dockerfile .
 sudo docker run -i --rm -p 3000:3000 docker-test
