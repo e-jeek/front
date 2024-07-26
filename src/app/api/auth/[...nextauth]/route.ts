3e072f3573054c832e20f2d058024849ef25fee9
bondyuu <87316155+bondyuu@users.noreply.github.com>
2024-07-26 10:54:48 +0900

    Fix: 환경변수 타입 처리

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,12 +5,12 @@ import NaverProvider from 'next-auth/providers/naver';
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-            clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
         }),
         NaverProvider({
-            clientId: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID,
-            clientSecret: process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET,
+            clientId: process.env.NAVER_CLIENT_ID ?? "",
+            clientSecret: process.env.NAVER_CLIENT_SECRET ?? "",
         })
     ],
 });
