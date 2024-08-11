352c43dce3bf653fe190626e58182b1380408ffe
bondyuu <87316155+bondyuu@users.noreply.github.com>
2024-08-11 15:03:26 +0900

    feat: axios 헤더 토큰 설정

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
+import {getCookie} from "cookies-next";
+import {headers} from "next/headers";
 
 const apiInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL?? "",
     headers: {
         "Content-Type": "application/json",
+        "Authorization": "Bearer " + getCookie("workoutwith_access_token")??""
     },
     withCredentials: true,
 });
